567a119727ca6ded34096f3e7debe66a043e7a99
Michael Schwarz <michi.schwarz@gmail.com>
2019-05-02 22:40:17 +0200

    Add knurled ring nut

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -32,6 +32,13 @@ object Main extends App {
     writePart(simpleBolt(thread, head, screwLength), s"$name-screw")
   }
 
+  def writeKnurledRingNut(name: String, screw: Screw): Unit = {
+    val thread = ISO.isoThread(screw.size, screw.pitch)
+    val head = Stuff.knurledRoundedRingNut(screw.headSize * 3 / 2, screw.headSize * 4 / 8)
+
+    writePart(nut(thread, head), s"$name-ring-nut")
+  }
+
   def writeWasher(name: String, washer: Washer): Unit =
     writePart(washer.surface, s"$name-washer")
 
@@ -44,8 +51,11 @@ object Main extends App {
     writeNutAndScrew("M8", Screw(8, 1.25, 13), 16)
 
     writeNutAndScrew("M10", Screw(10, 1.5, 16), 20)
+    writeKnurledRingNut("M10", Screw(10, 1.5, 16))
+
     writeNutAndScrew("M20", Screw(20, 2.5, 30), 40)
     writeNutAndScrew("M24", Screw(24, 3, 36), 50)
+    writeKnurledRingNut("M24", Screw(24, 3, 36))
 
     writeWasher("M8", Washer(8.4, 16, 1.6))
     writeWasher("M10", Washer(10.5, 20, 2))
---
+++ src/main/scala/Stuff.scala
@@ -0,0 +1,61 @@
+import ch.feuermurmel.nutsandbolts.knurl.{Knurl, KnurlMode, KnurlPattern, KnurlShape}
+import ch.feuermurmel.nutsandbolts.part.{BoltHead, Facing}
+import ch.feuermurmel.nutsandbolts.surface.Surface.{cylinder, sphere}
+import ch.feuermurmel.nutsandbolts.util.MathUtil.tau
+
+object Stuff {
+  /**
+    * @param size The size of a bolt head for the same size of bolt.
+    */
+  def knurledRingNut(size: Double) = {
+    val radius = size / 2
+
+    val shape = KnurlShape(0, 0.5, 1)
+
+    val pattern = KnurlPattern.forRidgeCount(
+      angle = tau / 12,
+      workpieceRadius = radius,
+      ridges = 15)
+
+    val knurl = Knurl(
+      pattern,
+      shape,
+      KnurlMode.BothSubtractive)
+
+    new BoltHead {
+      override def surface = knurl.applyToSurface(cylinder(radius))
+      override def cutoffFacing = Facing.outsideChamfer(radius - knurl.depth)
+      override def nominalHeight = size / 4
+    }
+  }
+
+  /**
+    * @param size The size of a bolt head for the same size of bolt.
+    */
+  def knurledRoundedRingNut(size: Double, height: Double) = {
+    val radius = size / 2
+
+    val shape = KnurlShape(0, 0.5, 1)
+
+    val pattern = KnurlPattern.forApproximateAdvance(
+      angle = tau / 12,
+      workpieceRadius = radius,
+      approximateAdvance = height / 3)
+
+    val knurl = Knurl(
+      pattern,
+      shape,
+      KnurlMode.BothSubtractive)
+
+    val sphereRadius = height * 2 / 3
+
+    val nutSurface = sphere(sphereRadius).grow(radius - sphereRadius)
+
+    new BoltHead {
+      // Shift knurled nut here so that the knurling is symmetric along the Z-axis.
+      override def surface = knurl.applyToSurface(nutSurface).shift(height / 2)
+      override def cutoffFacing = Facing.none
+      override def nominalHeight = height
+    }
+  }
+}
